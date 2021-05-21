cf9f379fc714890ce73f7311329cd82208003bc5
Jorge <jdieguezbean@gmail.com>
2021-05-21 07:56:09 +0300

    Adding crondata images to docker compose

--- postgres/init.sh
+++ postgres/init.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER crondata;
-    CREATE DATABASE cron_data;
-    GRANT ALL PRIVILEGES ON DATABASE cron_data TO docker;
-EOSQL
+psql -u $POSTGRES_USER CREATE DATABASE $POSTGRES_DB;
